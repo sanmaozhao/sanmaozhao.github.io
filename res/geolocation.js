9a30252674d68ed5a34a86fc40c77586ca18b641
Sanmao Zhao <sanmaozhao@gmail.com>
2020-10-09 15:49:53 +0800

    Update geolocation.js

--- res/geolocation.js
+++ res/geolocation.js
@@ -1,8 +1,8 @@
 navigator.geolocation.getCurrentPosition = function(a, b) {
 	a({
 		coords: {
-			latitude: 40.022061 + (Math.random()-0.5)*0.001,
-			longitude: 116.472126 + (Math.random()-0.5)*0.001
+			latitude: 40.021061 + (Math.random()-0.5)*0.001,
+			longitude: 116.466126 + (Math.random()-0.5)*0.001
 		},
 		timestamp: Date.now()
 	})
