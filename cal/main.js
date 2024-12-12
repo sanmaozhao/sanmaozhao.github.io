a8e9a449b4676e792d3288c4937eb411872673ea
Sanmao Zhao <sanmaozhao@gmail.com>
2024-12-12 11:29:23 +0800

    u

--- cal/main.js
+++ cal/main.js
@@ -18,17 +18,71 @@ try{
 Vue.use(vueTouchEvents,{
   swipeTolerance: 100,
 })
+
+events = events.map(e=>{
+  if(!e.class) e.class="food"
+  e.end = e.start
+  return e
+})
+
+const eventsStat = {}
+events.forEach(event=>{
+  const type = event.class.split(" ")[0]
+
+  if(!eventsStat[type]) eventsStat[type] ={}
+
+  const title = event.title||'s'
+  if(!eventsStat[type][title]){
+    eventsStat[type][title] ={
+      count:1,
+      dates:[event.start]
+    }
+  }else{
+    eventsStat[type][title].count ++
+    eventsStat[type][title].dates.push(event.start)
+  }
+})
+
+events.forEach(event=> event.title= (event.title||'') + (event.subTitle||''))
+
+const eventsStatArr = {}
+for (let [key, value] of Object.entries(eventsStat)) {
+
+  const events = []
+  for (let [name, event] of Object.entries(value)) {
+    events.push({
+      name,
+      count:event.count,
+      dates:event.dates
+    })
+  }
+  eventsStatArr[key] = events.sort((a,b)=>b.count - a.count)
+}
+
 var app = new Vue({
   el: '#app',
   components: { VueCal: vuecal },
   data: {
     selectedDate:events[events.length-1].start,
+    maxDate:events[events.length-1].start,
     minDate:events[0].start,
-    events:events.map(e=>{
-      if(!e.class) e.class="food"
-      e.end = e.start
-      return e
-    })
+    events,
+    mode:'cal',
+    eventsStatArr,
+    selectedTab:'food',
+    eventsStatIndex:0,
+    sheetVisible:false
+  },
+  computed:{
+    eventsStatDates(){
+      return this.eventsStatArr[this.selectedTab][this.eventsStatIndex].dates.map(d=>{
+        return {
+          name:d + ' ' + '日一二三四五六'.charAt(new Date(d).getDay()),
+          date:d,
+          method:this.gotoDate
+        }
+      })
+    }
   },
   methods:{
     swipeHandler(evt){
@@ -37,6 +91,22 @@ var app = new Vue({
       }else{
         this.$refs.cal.previous()
       }
+    },
+    switchMode(mode){
+      this.mode = mode
+    },
+    gotoDate(item){
+      this.sheetVisible = false
+      this.selectedDate = item.date
+      this.mode = 'cal'
+    },
+    onEventClick(evt){
+      const type = evt.class.split(" ")[0]
+      this.eventsStatIndex = this.eventsStatArr[type].findIndex(e=>{
+        return evt.title.startsWith(e.name)
+      })
+      this.selectedTab = type
+      setTimeout(()=>this.sheetVisible = true,10)
     }
   }
 })
